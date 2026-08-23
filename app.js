ca65645763dd12c762230d6ec33bf13c514b5b23
NielsE02 <71512277+NielsE02@users.noreply.github.com>
2026-08-23 04:37:27 +0200

    Increase limit of drinks selection to 2500

--- app.js
+++ app.js
@@ -175,7 +175,7 @@
       .from("drinks")
       .select("id, player_id, owner_user_id, drink_type, created_at, players(name)")
       .order("created_at", { ascending: false })
-      .limit(500);
+      .limit(2500);
 
     if (error) {
       throw error;
